4d6aef8144c4bd94f8f143ee5d7c962c498e2d1c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 19:59:45 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numero;
+ 
+ 
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+ 
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você digitou um número negativo.");
+    Console.ResetColor();
+}
+else
+{
+    Console.WriteLine();
+}
